82657167b99a2d4cd14e3213637ecb183a27dfaa
jab9643 <jab9643@nyu.edu>
2020-10-01 20:10:51 -0400

    fix http header packet

--- main.py
+++ main.py
@@ -23,8 +23,7 @@ def webServer(port=13331):
 
             #Send one HTTP header line into socket
             #Fill in start
-            connectionSocket.send('\HTTP/1.1 200 OK\n\n')
-            connectionSocket.send(outputdata)
+            connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
             #Fill in end
 
             #Send the content of the requested file to the client
@@ -36,7 +35,7 @@ def webServer(port=13331):
         except IOError:
             #Send response message for file not found (404)
             #Fill in start
-            connectionSocket.send('\HTTP/1.1 404 Not Found\n\n')
+            connectionSocket.send('HTTP/1.1 404 Not Found\r\n\r\n'.encode())
             #Fill in end
 
             #Close client socket
